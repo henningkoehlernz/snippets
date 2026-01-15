d78ea838f18bfaf3ee390286e271d0afa8ed877b
Henning Koehler <henning.koehler.nz@gmail.com>
2026-01-15 18:03:18 +1300

    made Factorial template-based

--- fac.cpp
+++ fac.cpp
@@ -4,46 +4,44 @@
  * dynamic programming approach to fractional factorial computation;
  * no divisions so that unnecessary range overflow is avoided
  */
+template<typename T>
 class Factorial
 {
     // store values to avoid recomputation
-    static std::vector<std::vector<double>> known;
+    static std::vector<std::vector<T>> known;
 public:
     // returns x! / (x-delta)!
-    static double fraction(int x, int delta);
+    static T fraction(int x, int delta)
+    {
+        if (known.size() <= x)
+            known.resize(x + 1, { 1 });
+        if (known[x].size() <= delta)
+        {
+            int y = x, y_delta = delta;
+            while (known[y-1].size() <= y_delta - 1)
+            {
+                y--; y_delta--;
+            }
+            while (y <= x)
+            {
+                for (int d = known[y].size(); d <= y_delta; d++)
+                    known[y].push_back(known[y-1][d-1] * y);
+                y++; y_delta++;
+            }
+        }
+        return known[x][delta];
+    }
     // returns x!
-    static double of(int x);
+    static T of(int x)
+    {
+        return fraction(x, x);
+    }
 };
 
+template<typename T>
+std::vector<std::vector<T>> Factorial<T>::known;
+
 using namespace std;
-
-vector<vector<double>> Factorial::known;
-
-double Factorial::fraction(int x, int delta)
-{
-    if (known.size() <= x)
-        known.resize(x + 1, { 1 });
-    if (known[x].size() <= delta)
-    {
-        int y = x, y_delta = delta;
-        while (known[y-1].size() <= y_delta - 1)
-        {
-            y--; y_delta--;
-        }
-        while (y <= x)
-        {
-            for (int d = known[y].size(); d <= y_delta; d++)
-                known[y].push_back(known[y-1][d-1] * y);
-            y++; y_delta++;
-        }
-    }
-    return known[x][delta];
-}
-
-double Factorial::of(int x)
-{
-    return fraction(x, x);
-}
 
 int main(int argc, char**argv)
 {
@@ -53,6 +51,8 @@ int main(int argc, char**argv)
         return 0;
     }
     int x = atoi(argv[1]), delta = atoi(argv[2]);
-    cout << x << "! / (" << x << "-" << delta << ")! = " << Factorial::fraction(x, delta) << endl;
+    cout << x << "! / (" << x << "-" << delta << ")! =" << endl;
+    cout << "\t= " << Factorial<double>::fraction(x, delta) << " (using double)" << endl;
+    cout << "\t= " << Factorial<uint64_t>::fraction(x, delta) << " (using uint64_t)" << endl;
     return 0;
 }
